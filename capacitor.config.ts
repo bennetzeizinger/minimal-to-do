ecc75b8bfe51d894a031118d47643537b5a51eff
Aidexe <bennet.zeizinger@gmail.com>
2024-04-16 18:26:24 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Minimal ToDo',
+  webDir: 'www'
+};
+
+export default config;
